f2897f75df52a23ef5a471659bd49bd6c9a9b36c
Some <edenincooperated@gmail.com>
2022-06-07 08:15:27 +0100

    a

--- QuickModToTrainModConverter/__init__.py
+++ QuickModToTrainModConverter/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Eden",
     "description" : "",
     "blender" : (3, 0, 0),
-    "version" : (0, 0, 5, 11),
+    "version" : (0, 0, 5, 12),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
